a4937ed812d3cf16c219dedeb2f681630d614980
elsif84 <mortifuce@yahoo.fr>
2024-07-10 13:38:46 +0200

    ajout texte alt

--- scripts/array.js
+++ scripts/array.js
@@ -4,188 +4,215 @@ let heroes = [
         initials: "Z----",
         unrevealed: "img/Hero1u.png",
         revealed: "img/Hero1r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°1"
     },
     {
         name: "Link",
         initials: "L---",
         unrevealed: "img/Hero2u.png",
         revealed: "img/Hero2r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°2"
     },
     {
         name: "Ganon",
         initials: "G----",
         unrevealed: "img/Hero14u.png",
         revealed: "img/Hero14r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°14"
     },
     {
         name: "Impa",
         initials: "I---",
         unrevealed: "img/Hero3u.png",
         revealed: "img/Hero3r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°3"
     },
     {
         name: "Rhoam Bosphoramus Hyrule",
         initials: "R---- B---------- H-----",
         unrevealed: "img/Hero4u.png",
         revealed: "img/Hero4r.png",
-        class: "hero4Name"
+        class: "hero4Name",
+        alt: "Personnage n°4"
     },
     {
         name: "Mipha",
         initials: "M----",
         unrevealed: "img/Hero5u.png",
         revealed: "img/Hero5r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°5"
     },
     {
         name: "Revali",
         initials: "R-----",
         unrevealed: "img/Hero6u.png",
         revealed: "img/Hero6r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°6"
     },
     {
         name: "Urbosa",
         initials: "U-----",
         unrevealed: "img/Hero7u.png",
         revealed: "img/Hero7r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°7"
     },
     {
         name: "Daruk",
         initials: "D----",
         unrevealed: "img/Hero8u.png",
         revealed: "img/Hero8r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°8"
     },
     {
         name: "Pru'Ha",
         initials: "P--'H-",
         unrevealed: "img/Hero17u.png",
         revealed: "img/Hero17r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°17"
     },
     {
         name: "Faras",
         initials: "F----",
         unrevealed: "img/Hero18u.png",
         revealed: "img/Hero18r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°18"
     },
     {
         name: "Noïa",
         initials: "N---",
         unrevealed: "img/Hero26u.png",
         revealed: "img/Hero26r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°26"
     },
     {
         name: "Asarim",
         initials: "A-----",
         unrevealed: "img/Hero16u.png",
         revealed: "img/Hero16r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°16"
     },
     {
         name: "Rauru",
         initials: "R----",
         unrevealed: "img/Hero22u.png",
         revealed: "img/Hero22r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°22"
     },
     {
         name: "Sonia",
         initials: "S----",
         unrevealed: "img/Hero23u.png",
         revealed: "img/Hero23r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°23"
     },
     {
         name: "Mineru",
         initials: "M-----",
         unrevealed: "img/Hero24u.png",
         revealed: "img/Hero24r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°24"
     },
     {
         name: "Ganondorf",
         initials: "G--------",
         unrevealed: "img/Hero15u.png",
         revealed: "img/Hero15r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°15"
     },
     {
         name: "Sidon",
         initials: "S----",
         unrevealed: "img/Hero9u.png",
         revealed: "img/Hero9r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°9"
     },
     {
         name: "Teba",
         initials: "T---",
         unrevealed: "img/Hero10u.png",
         revealed: "img/Hero10r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°10"
     },
     {
         name: "Babil",
         initials: "B----",
         unrevealed: "img/Hero27u.png",
         revealed: "img/Hero27r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°27"
     },
     {
         name: "Riju",
         initials: "R---",
         unrevealed: "img/Hero11u.png",
         revealed: "img/Hero11r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°11"
     },
     {
         name: "Yunobo",
         initials: "Y-----",
         unrevealed: "img/Hero12u.png",
         revealed: "img/Hero12r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°12"
     },
     {
         name: "Grand Kohga",
         initials: "G---- K----",
         unrevealed: "img/Hero13u.png",
         revealed: "img/Hero13r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°13"
     },
     {
         name: "Suppa",
         initials: "S----",
         unrevealed: "img/Hero20u.png",
         revealed: "img/Hero20r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°20"
     },
     {
         name: "Penn",
         initials: "P---",
         unrevealed: "img/Hero25u.png",
         revealed: "img/Hero25r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°25"
     },
     {
         name: "Terrako",
         initials: "T------",
         unrevealed: "img/Hero19u.png",
         revealed: "img/Hero19r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°19"
     },
     {
         name: "Astor",
         initials: "A----",
         unrevealed: "img/Hero21u.png",
         revealed: "img/Hero21r.png",
-        class: "heroName"
+        class: "heroName",
+        alt: "Personnage n°21"
     }
 ]
